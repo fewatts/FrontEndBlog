a94bf37b984d1ba131cc0b256102425ed689e83f
Fewatts <feralveswatts@gmail.com>
2023-04-12 18:30:18 -0300

    Task 3
    NavBar, Footer e Home OK

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,14 +1,51 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { Box } from '@mui/material';
+
 function NavBar(){
     return(
-        <div className="navbar">
-            {/* <ul>
-                <li>opção 1</li>
-                <li>opção 2</li>
-                <li>opção 3</li>
-                <li>opção 4</li>
-            </ul> */}
-        </div>
-    )
+        <>
+        <AppBar position="static" style={{background: 'var(--background)'}}>
+          <Toolbar variant="dense" >
+            <Box display={'flex'} justifyContent={'space-between'} width={'100%'} >
+              <Box style={{ cursor: 'pointer' }}>
+                <Typography variant="h5" color="inherit">
+                  <strong>Blog DJ's</strong>
+                </Typography>
+              </Box>
+  
+              <Box display="flex" justifyContent="start">
+                <Box mx={1} style={{ cursor: 'pointer' }}>
+                  <Typography variant="h6" color="inherit">
+                    home
+                  </Typography>
+                </Box>
+                <Box mx={1} style={{ cursor: 'pointer' }}>
+                  <Typography variant="h6" color="inherit">
+                    postagens
+                  </Typography>
+                </Box>
+                <Box mx={1} style={{ cursor: 'pointer' }}>
+                  <Typography variant="h6" color="inherit">
+                    temas
+                  </Typography>
+                </Box>
+                <Box mx={1} style={{ cursor: 'pointer' }}>
+                  <Typography variant="h6" color="inherit">
+                    cadastrar tema
+                  </Typography>
+                </Box>
+                <Box mx={1} style={{ cursor: 'pointer' }}>
+                  <Typography variant="h6" color="inherit">
+                    logout
+                  </Typography>
+                </Box>
+              </Box>
+            </Box>
+          </Toolbar>
+        </AppBar>
+      </>
+    );
 }
 
 export default NavBar
--- src/paginas/Home.tsx
+++ src/paginas/Home.tsx
@@ -1,11 +1,64 @@
+import React from 'react';
+import { Typography, Grid, Button } from '@material-ui/core';
+import { Box } from '@mui/material';
 import './Home.css';
-import logoBlog from '/logoBlog.png'
 
 function Home() {
     return (
       <>
-        <div className='titulo'> <h1>Home</h1></div>
-        <div className='imgdiv'><img src={logoBlog} alt="LogoBlog" className='img'/></div>
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        style={{ backgroundColor: 'var(--blue)' }}
+      >
+        <Grid item xs={6}>
+          <img
+            src="https://i.imgur.com/Waz7fBM.png"
+            alt=""
+            className='fthome'
+          />
+        </Grid>
+        <Grid alignItems="center" item xs={6}>
+          <Box paddingX={20}  >
+            <Typography
+              variant="h3"
+              gutterBottom
+              color="textPrimary"
+              component="p"
+              align="center"
+              style={{ color: 'white', fontWeight: 'bold' }}
+            >
+              Seja bem vinde!
+            </Typography>
+            <Typography
+              variant="h5"
+              gutterBottom
+              color="textPrimary"
+              component="h5"
+              align="center"
+              style={{ color: 'white', fontWeight: 'bold' }}
+            >
+              Poste suas experiências com a música e mixagem aqui!
+            </Typography>
+          </Box>
+          <Box display="flex" justifyContent="center">
+            <Box marginRight={1}></Box>
+            <Button
+              variant="outlined"
+              style={{
+                borderColor: 'white',
+                backgroundColor: 'var(--blue)',
+                color: 'white',
+              }}
+            >
+              Ver Postagens
+            </Button>
+          </Box>
+        </Grid>
+        <Grid xs={12} style={{ backgroundColor: 'white' }}></Grid>
+      </Grid>
     </>
     );
   }  
